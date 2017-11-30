5b02955dca717ec3274f22b77c499973a55e487f
Tito <titoconte@users.noreply.github.com>
2017-11-30 11:53:43 -0200

    Os.path.join added

--- StartProject.py
+++ StartProject.py
@@ -4,24 +4,82 @@ import os
 
 def StartProject():
 
-    root = raw_input('Entre com o diretorio onde o projeto sera iniciado: ')
+    root = input('Entre com o diretorio onde o projeto sera iniciado: ')
     MainDir = os.path.join(root,'Operacao','Tratamento_e_Analise')
     Produtos = os.path.join(root,'Produtos')
-    os.makedirs(MainDir,'Figuras','MetOcean','Correntes')
-    os.makedirs(MainDir.replace('Tratamento_e_Analise','Docs'),'Relatorio')
-    os.makedirs(MainDir,'Figuras','MetOcean','Densidade')
-    os.makedirs(MainDir,'Figuras','Deposicao')
-    os.makedirs(MainDir,'Figuras','Pluma')
-    os.makedirs(MainDir,'Sig','Resultados','Individuais')
-    os.makedirs(MainDir,'Sig','Resultados','Somas')
-    os.makedirs(MainDir,'Sig','Resultados','Integrados')
-    os.makedirs(MainDir,'Sig','PontoDeModelagem')
-    os.makedirs(MainDir,'Diagnosticos')
-    os.makedirs(MainDir,'Scripts')
-    os.makedirs(MainDir,'Loc_Data')
-    os.makedirs(Produtos,'Documento')
-    os.makedirs(Produtos,'Ilustracoes')
-    os.makedirs(Produtos,'Shapes')
+    os.makedirs(os.path.join(
+        MainDir,
+        'Figuras',
+        'MetOcean',
+        'Correntes'
+        ))
+    os.makedirs(os.path.join(
+        MainDir.replace('Tratamento_e_Analise','Docs'),
+        'Relatorio'
+        ))
+    os.makedirs(os.path.join(
+        MainDir,
+        'Figuras',
+        'MetOcean',
+        'Densidade'
+        ))
+    os.makedirs(os.path.join(
+        MainDir,
+        'Figuras',
+        'Deposicao'
+        ))
+    os.makedirs(os.path.join(
+        MainDir,
+        'Figuras',
+        'Pluma'
+        ))
+    os.makedirs(os.path.join(
+        MainDir,
+        'Sig',
+        'Resultados',
+        'Individuais'
+        ))
+    os.makedirs(os.path.join(
+        MainDir,
+        'Sig',
+        'Resultados',
+        'Somas'
+        ))
+    os.makedirs(os.path.join(
+        MainDir,
+        'Sig',
+        'Resultados',
+        'Integrados'
+        ))
+    os.makedirs(os.path.join(
+        MainDir,
+        'Sig',
+        'PontoDeModelagem'
+        ))
+    os.makedirs(os.path.join(
+        MainDir,
+        'Diagnosticos'
+        ))
+    os.makedirs(os.path.join(
+        MainDir,
+        'Scripts'
+        ))
+    os.makedirs(os.path.join(
+        MainDir,
+        'Loc_Data'
+        ))
+    os.makedirs(os.path.join(
+        Produtos,
+        'Documento'
+        ))
+    os.makedirs(os.path.join(
+        Produtos,
+        'Ilustracoes'
+        ))
+    os.makedirs(os.path.join(
+        Produtos,
+        'Shapes'
+        ))
 
 if __name__ == '__main__':
 
